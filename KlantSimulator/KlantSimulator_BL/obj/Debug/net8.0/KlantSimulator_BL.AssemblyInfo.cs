123c04deaed2f9c38e4fd1ad62d4a82b77f002d1
XilaADL <162587142+XilaADL@users.noreply.github.com>
2024-10-09 21:40:24 +0200

    prutsen

--- KlantSimulator/KlantSimulator_BL/obj/Debug/net8.0/KlantSimulator_BL.AssemblyInfo.cs
+++ KlantSimulator/KlantSimulator_BL/obj/Debug/net8.0/KlantSimulator_BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KlantSimulator_BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c26ee286c65a471bbd9084cd0ef11ebfd3c8800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597ad35ade65851ccaadb769306cb388ae207ea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("KlantSimulator_BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KlantSimulator_BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
